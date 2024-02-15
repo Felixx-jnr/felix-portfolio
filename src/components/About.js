25694f59b6131637fcf105cae049768bd1646eef
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-15 16:36:09 +0100

    setup

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,22 @@
-import me from '../assets/edit me.png'
+import me from '../assets/e.png'
 
 const About = () => {
   return ( 
+
+    
+
     <div className="about">
+
+      <div className='about__image'>
+        <img src={me} alt="" />
+      </div>
+
       <div className='about__description'>
         <h1 className='about__description--header'>Frontend developer</h1>
         <p className='about__description--writeup'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ipsam illum consectetur aliquam dolores, nesciunt laudantium tempore quisquam quas molestias, sed labore nulla quae accusantium. Sed tempore iusto magni cupiditate.</p>
       </div>
 
-      <div className='about__image'>
-        <img src={me} alt="" />
-      </div>
+      
     </div>
    );
 }
