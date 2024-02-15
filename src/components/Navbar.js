05f97e950fcebde729acc44f55e65b8b32afdac6
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-15 23:31:52 +0100

    setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,25 @@
 const Navbar = () => {
   return ( 
     <nav className="navbar">
+
       <div>
         <header>felix udoh</header>
       </div>
 
-      <div className="links">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li className="hire">Hire Me</li>
-        </ul>
-      </div>
-
+      <ul className="links pages">
+        <li>Home</li>
+        <li>About</li>
+        <li>Stacks</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+  
+      <ul className="links icons">
+        <li>git</li>
+        <li>Twitter</li>
+        <li>Linkedn</li>
+      </ul>
+    
     </nav> 
   );
 }
