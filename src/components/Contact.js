f228f929349a568347bde0452cc34164a4b22954
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-16 23:13:36 +0100

    contact Setup

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,25 @@
+import robo2 from '../assets/robo2.png'
+
 const Contact = () => {
   return ( 
     <div className="contact">
 
-    <h1 className='contact__header'>CONTACT ME</h1>
+      <h1 className='contact__header'>CONTACT ME</h1>
 
-    <div className='contact__info'>
+      <div className='contact__info'>
 
-      <div className="contact__info--writeup">
-        <h2>Contact Felix ! ! !</h2>
+        <div className="contact__info--image">
+          <img src={robo2} alt="about felix udoh" />
+        </div>
+
+        <div className="contact__info--writeup">
+          <h2>Contact Felix ! ! !</h2>
+        </div>
+      
       </div>
+    </div>
 
-    </div>
-  
-    </div>
+
   );
 }
  
