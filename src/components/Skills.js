a88eea49bb304e5e59fbdd7be16877ed13fd2ef6
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-17 21:54:17 +0100

    card

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,18 +2,20 @@ import { faCss3, faHtml5, faJs, faNodeJs, faReact, faSass } from '@fortawesome/f
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Skills = () => {
+
+
   return ( 
     <div className="skills">
 
       <h1 className='skills__header'>SKILLS</h1>
 
       <div className="skills__icon">
-        <div><li><FontAwesomeIcon icon={faHtml5} /></li></div>
-        <li><FontAwesomeIcon icon={faCss3} /></li>
-        <li><FontAwesomeIcon icon={faJs} /></li>
-        <li><FontAwesomeIcon icon={faReact} /></li>
-        <li><FontAwesomeIcon icon={faNodeJs} /></li>
-        <li><FontAwesomeIcon icon={faSass} /></li>
+        <li className='card'><FontAwesomeIcon icon={faHtml5} /></li>
+        <li className='card'><FontAwesomeIcon icon={faCss3} /></li>
+        <li className='card'><FontAwesomeIcon icon={faJs} /></li>
+        <li className='card'><FontAwesomeIcon icon={faReact} /></li>
+        <li className='card'><FontAwesomeIcon icon={faNodeJs} /></li>
+        <li className='card'><FontAwesomeIcon icon={faSass} /></li>
       </div>
        
       <div className="blob"></div>
