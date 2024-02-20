54b9f221328c3e2f5fda06b23768e2d9915490d3
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-20 19:24:18 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,39 @@
 import { faGithub, faXTwitter, faFacebook} from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Footer = () => {
   return ( 
     <div className='footer'>
-      <div className='footer__overlay footer__setup'>
+      <div className="footer__overlay">
+        <div className='footer__setup'>
 
-        <h2 className="footer__name">FELIX UDOH</h2>
+          <h2 className="footer__name">FELIX UDOH</h2>
 
-        <div className="footer__contact">
-          <h2>LINKS</h2>
-          <li>Home</li>
-          <li>About</li>
-          <li>Skills</li>
-          <li>Projects</li>
-          <li>Contact</li>
-        </div>
-
-        <div className="footer__links">
-          <h2>CONTACTS</h2>
-          <div>
-            <span>+234 705 771 8692</span>
-            <span>uyuoukoh@gmail.com</span>
+          <div className="footer__links">
+            <h2>LINKS</h2>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Projects</li>
+            <li>Contact</li>
           </div>
-          <div><FontAwesomeIcon icon={faEnvelope}/></div>
-          <div><FontAwesomeIcon icon={faGithub}/></div>
-          <div><FontAwesomeIcon icon={faXTwitter}/></div>
-          <div><FontAwesomeIcon icon={faFacebook}/></div>
+
+          <div className="footer__contact">
+            <h2>CONTACTS</h2>
+            <div className='main'>
+              <li>+234 705 771 8692</li>
+              <li>UYUOUKOH@GMAIL.COM</li>
+            </div>
+
+            <div className='icon'>
+              <div><FontAwesomeIcon icon={faGithub}/></div>
+              <div><FontAwesomeIcon icon={faXTwitter}/></div>
+              <div><FontAwesomeIcon icon={faFacebook}/></div>
+            </div>
+          </div>
         </div>
-
-
-       
       </div>
+
     </div>
 
    );
