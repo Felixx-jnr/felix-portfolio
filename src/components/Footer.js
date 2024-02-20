8d0dea9f3067e9bff8f90e60aea5c5aefaf82be6
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-20 18:10:47 +0100

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Footer = () => {
   return ( 
     <div className='footer'>
-      <div className='glass new'>
-        <div><FontAwesomeIcon icon={faEnvelope}/><span>Uyuoukoh@gmail.com</span></div>
-        <div><FontAwesomeIcon icon={faGithub}/><span>Felix_jnr</span></div>
-        <div><FontAwesomeIcon icon={faXTwitter}/><span>Felix_jnr</span></div>
-        <div><FontAwesomeIcon icon={faFacebook}/><span>Felix Udoh</span></div>
+      <div className='footer__overlay footer__setup'>
+
+        <h2 className="footer__name">FELIX UDOH</h2>
+
+        <div className="footer__contact">
+          <h2>LINKS</h2>
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </div>
+
+        <div className="footer__links">
+          <h2>CONTACTS</h2>
+          <div>
+            <span>+234 705 771 8692</span>
+            <span>uyuoukoh@gmail.com</span>
+          </div>
+          <div><FontAwesomeIcon icon={faEnvelope}/></div>
+          <div><FontAwesomeIcon icon={faGithub}/></div>
+          <div><FontAwesomeIcon icon={faXTwitter}/></div>
+          <div><FontAwesomeIcon icon={faFacebook}/></div>
+        </div>
+
+
+       
       </div>
     </div>
+
    );
 }
  
