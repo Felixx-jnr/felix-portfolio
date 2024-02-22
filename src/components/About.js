1cbadb79a7390f00a9455a3ddd3bc349e82ff270
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-22 03:49:30 +0100

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,33 @@
-import robo from '../assets/robo.png'
-
 const About = () => {
   return ( 
     <div className="about" id='about'>
 
       <h1 className='about__header'>ABOUT</h1>
+      <h2 className='about__intro'>Welcome to Felix's Frontend Portfolio! </h2>
+      <p>I am a versatile developer with a strong focus on frontend development and a solid understanding of backend technologies. Specializing in the MERN stack (MongoDB, Express.js, React, Node.js), I bring a unique blend of skills to the table, allowing me to create full-stack solutions that are both elegant and robust.</p>
 
       <div className='about__info'>
 
-        <div className="about__info--writeup">
-          <h2>About Felix ! ! !</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis quisquam quos nobis odio, reiciendis exercitationem explicabo corporis ducimus sint omnis atque iure deleniti sunt id deserunt. Dolore provident fugit blanditiis ipsam maxime odit, mollitia, distinctio delectus quos quibusdam id alias?</p>
+        <div className="about__info--body">
+          <div className='about__info--glass'>
+            <p>ybh i i h i</p>
+          </div>
         </div>
 
-        <div className="about__info--image">
-          <img src={robo} alt="about felix udoh" />
+        <div className="about__info--body">
+          <div className='about__info--glass'>
+            <p>ybh i i h i</p>
+          </div>
         </div>
+      </div>
 
-
+      <div className="connect">
+        <h2 className="connet">Let's Connect</h2>
+        <p>Whether you're looking to revamp your web presence, streamline your workflows, or tackle a new challenge, I'm here to help. Let's collaborate and bring your ideas to life!</p>
       </div>
     
     </div>
-   );
+  );
 }
  
 export default About;
