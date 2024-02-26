fd3d1114072b172da084f3af2e991d5827d9cb5c
Felixx-jnr <uyuoukoh@gmail.com>
2024-02-26 12:36:43 +0100

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { faFacebook, faGithub, faLinkedinIn, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedinIn, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRef } from 'react';
@@ -54,54 +54,5 @@ function Navbar() {
 		</header>
 	);
 }
-
-// const Navbar = () => {
-
-//   const navRef = useRef();
-
-//   const showNavbar = () => {
-//     navRef.current.classList.toggle('responsive_nav')
-//   }
-
-//   return ( 
-    
-//     <header >
-//       <h2>Felix</h2>
-
-//       <nav className='navbar' ref = {navRef}>
-//         <ul className="links pages">
-//           <li><AnchorLink href="#home">Home</AnchorLink></li>
-//           <li><AnchorLink href="#about">About</AnchorLink></li>
-//           <li><AnchorLink href="#skills">Skills</AnchorLink></li>
-//           <li><AnchorLink href="">Projects</AnchorLink></li>
-//           <li><AnchorLink href="#contact">Contact</AnchorLink></li>
-//         </ul>
-    
-//         <ul className="links icons">
-//           <a href="mailto:uyuoukoh@gmail.com?subject=Let's%20Work&body=Hey%2C%20Good%20day%2C%20My%20name%20is..." target='blank'> <li><FontAwesomeIcon icon={faEnvelope} /></li></a>
-//           <a href="https://github.com/Felixx-jnr" target='blank'> <li><FontAwesomeIcon icon={faGithub} /></li></a>
-//           <a href="https://x.com/Felixx_jnr?t=ViRhuKymNfmS_2pyCgv_cw&s=09" target='blank'> <li><FontAwesomeIcon icon={faXTwitter} /></li></a>
-//           <a href="https://www.linkedin.com/in/uyuoukoh-udoh-305a782b5?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target='blank'> <li><FontAwesomeIcon icon={faLinkedinIn} /></li></a>
-//           <a href="https://www.facebook.com/profile.php?id=100073334363631&mibextid=ZbWKwL" target='blank'> <li><FontAwesomeIcon icon={faFacebook} /></li></a>
-
-//         </ul>
-
-//         <div>
-//           <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-//             <FaTimes/>
-//           </button>
-//           <button className='nav-btn' onClick={showNavbar}>
-//             <FaBars/>
-//           </button>
-
-//         </div>
-
-//       </nav>
-
-      
-//     </header> 
-    
-//   );
-// }
  
 export default Navbar;
