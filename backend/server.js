e174b68860954366c944fe95ce118e21860e1b1a
Felixx-jnr <uyuoukoh@gmail.com>
2024-04-12 22:54:00 +0100

    done

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
+dotenv.config();
 
 app.use(bodyParser.json());
 
@@ -17,7 +19,7 @@ app.post("/send-email", (req, res) => {
     service: "gmail",
     auth: {
       user: "uyuoukoh@gmail.com",
-      pass: "kiyj wvcq aezd takm",
+      pass: process.env.APP_PASS,
     },
   });
 
@@ -30,10 +32,8 @@ app.post("/send-email", (req, res) => {
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error("Error sending email:", error);
       res.status(500).send("Error sending email");
     } else {
-      console.log("Email sent:", info.response);
       res.status(200).send("Email sent successfully");
     }
   });
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,86 +1,3 @@
-// import {
-//   faGithub,
-//   faXTwitter,
-//   faFacebook,
-// } from "@fortawesome/free-brands-svg-icons";
-// import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-// import { faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-// const Contact = () => {
-//   const twitter = "https://x.com/Felixx_jnr?t=ViRhuKymNfmS_2pyCgv_cw&s=09";
-//   const facebook =
-//     "https://www.facebook.com/profile.php?id=100073334363631&mibextid=ZbWKwL";
-//   const email =
-//     "mailto:uyuoukoh@gmail.com?subject=Lets%20Work&body=Hey%2C%20Good%20day%2C%20My%20name%20is...";
-//   const github = "https://github.com/Felixx-jnr";
-//   return (
-//     <div className="contact">
-//       <h1
-//         id="contact"
-//         className="contact__header"
-//       >
-//         CONTACT
-//       </h1>
-
-//       <div className="contact__info">
-//         <FontAwesomeIcon
-//           className="contact__info--image"
-//           icon={faPhoneVolume}
-//         />
-
-//         <div className="contact__info--writeup">
-//           <h2>Contact Details ! ! !</h2>
-
-//           <div className="contact__info--logos">
-//             <div>
-//               <a
-//                 href={email}
-//                 target="blank"
-//               >
-//                 <FontAwesomeIcon icon={faEnvelope} />
-//                 <span>Uyuoukoh@gmail.com</span>
-//               </a>
-//             </div>
-
-//             <div>
-//               <a
-//                 href={github}
-//                 target="blank"
-//               >
-//                 <FontAwesomeIcon icon={faGithub} />
-//                 <span>Felix_jnr</span>
-//               </a>
-//             </div>
-
-//             <div>
-//               <a
-//                 href={twitter}
-//                 target="blank"
-//               >
-//                 <FontAwesomeIcon icon={faXTwitter} />
-//                 <span>Felix_jnr</span>
-//               </a>
-//             </div>
-
-//             <div>
-//               <a
-//                 href={facebook}
-//                 target="blank"
-//               >
-//                 <FontAwesomeIcon icon={faFacebook} />
-//                 <span>Felix Udoh</span>
-//               </a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
 import React, { useState } from "react";
 import axios from "axios";
 
@@ -109,14 +26,22 @@ const Contact = () => {
 
       setMessage(response.data);
 
+      setFormData({
+        name: "",
+        email: "",
+        message: "",
+      });
+
       setTimeout(() => {
         setMessage("");
+        setIsSending(false);
       }, 3000);
     } catch (error) {
       setMessage("Error sending email");
 
       setTimeout(() => {
         setMessage("");
+        setIsSending(false);
       }, 3000);
     }
   };
@@ -133,6 +58,7 @@ const Contact = () => {
       <form
         onSubmit={handleSubmit}
         className="contact__form"
+        id="contact"
       >
         <div>
           <label htmlFor="name">Name:</label>
