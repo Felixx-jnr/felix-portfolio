d54532673d315a2c49105d5fd8ed1a1198fa8123
Felixx-jnr <uyuoukoh@gmail.com>
2024-07-02 17:41:13 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,69 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const nodemailer = require("nodemailer");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+
+// const app = express();
+// dotenv.config(); // Make sure to configure dotenv before using environment variables
+
+// const PORT = process.env.PORT || 3001;
+
+// const allowedOrigins = [
+//   "https://felix-portfolio-wg48.onrender.com",
+
+//   "https://felixx-portfolio.onrender.com/",
+// ];
+
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
+
+// app.use(cors(corsOptions));
+// app.use(bodyParser.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Yes, it is good");
+// });
+
+// app.post("/send-email", (req, res) => {
+//   const { name, email, message } = req.body;
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: "uyuoukoh@gmail.com",
+//       pass: process.env.APP_PASS,
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: email,
+//     to: "uyuoukoh@gmail.com",
+//     subject: `Message from ${name}`,
+//     text: `From: ${email}\n\nMy name is ${name}\n\n${message}`,
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       res.status(500).send("Error sending email");
+//     } else {
+//       res.status(200).send("Email sent successfully");
+//     }
+//   });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
@@ -5,33 +71,16 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const app = express();
-dotenv.config(); // Make sure to configure dotenv before using environment variables
-
 const PORT = process.env.PORT || 3001;
 
-const allowedOrigins = [
-  "https://felix-portfolio-wg48.onrender.com",
-
-  "https://felixx-portfolio.onrender.com/",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+const corsOption = {
+  origin: ["*"],
 };
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
+app.use(cors(corsOption));
+dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Yes, it is good");
-});
+app.use(bodyParser.json());
 
 app.post("/send-email", (req, res) => {
   const { name, email, message } = req.body;
@@ -45,7 +94,7 @@ app.post("/send-email", (req, res) => {
   });
 
   const mailOptions = {
-    from: email,
+    from: `${email}`,
     to: "uyuoukoh@gmail.com",
     subject: `Message from ${name}`,
     text: `From: ${email}\n\nMy name is ${name}\n\n${message}`,
